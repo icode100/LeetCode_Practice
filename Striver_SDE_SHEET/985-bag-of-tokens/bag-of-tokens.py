6c63cb15398f66d9266af9c53544a75868053fe2
Ipsit Das <121373392+icode100@users.noreply.github.com>
2024-04-14 22:27:48 +0530

    Time: 57 ms (26.87%) | Memory: 16.8 MB (12.73%) - LeetSync

---
+++ Striver_SDE_SHEET/985-bag-of-tokens/bag-of-tokens.py
@@ -0,0 +1,27 @@
+from collections import deque
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        if not tokens:
+            return 0
+        max_score = 0
+        q = deque(sorted(tokens))
+        if power<q[0]:
+            return 0
+        faceup = True if power >= q[0] else False
+        score = 0
+        while q:
+            if faceup:
+                score+=1
+                max_score = max(score,max_score)
+                power-=q.popleft()
+                if q:
+                    faceup = False if power<q[0] else True
+            else:
+                score-=1
+                max_score = max(score,max_score)
+                power+=q.pop()
+                if q:
+                    faceup = False if power<q[0] else True
+        return max_score
+
+
