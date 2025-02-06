18a37c6d90007f00c4fd951eecfb97f3c05a5fc5
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-02-06 22:17:38 +0530

    Time: 237 ms (23.52%) | Memory: 40.9 MB (74.46%) - LeetSync

--- Striver_SDE_SHEET/2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
+++ Striver_SDE_SHEET/2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
@@ -1,19 +1,15 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        hashmap = defaultdict(int)
-        for start,end in flowers:
-            hashmap[start]+=1
-            hashmap[end+1]-=1
+        difference = SortedDict()
+        for s,e in flowers:
+            difference[s]=difference.get(s,0)+1
+            difference[e+1]=difference.get(e+1,0)-1
         pos = list()
-        prefix = list()
-        sumi = 0
-        for key in sorted(hashmap):
-            pos.append(key)
-            sumi+=hashmap[key]
-            prefix.append(sumi)
-        ans = list()
-        for i in people:
-            ans.append(prefix[bisect_right(pos,i)-1])
-        return ans
+        prefix = [0]
+        for k,v in difference.items():
+            pos.append(k)
+            prefix.append(prefix[-1]+v)
+        return [prefix[bisect_right(pos,i)] for i in people]
+
 
         
