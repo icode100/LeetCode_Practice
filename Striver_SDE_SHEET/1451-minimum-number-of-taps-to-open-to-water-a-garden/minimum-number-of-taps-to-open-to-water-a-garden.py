25be524f7b1fa401d6cb6c0b985c25b2ab890ecd
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-02-06 22:05:00 +0530

    Time: 31 ms (48.30%) | Memory: 18.3 MB (69.61%) - LeetSync

--- Striver_SDE_SHEET/1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
+++ Striver_SDE_SHEET/1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,23 +1,14 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        maxrange = [0]*(n+1)
-        for i,num in enumerate(ranges): maxrange[max(0,i-num)] = max(maxrange[max(0,i-num)],min(n,i+num))
-        next = 0
-        ans = 1
-        best = maxrange[0]
-        for i,num in enumerate(maxrange):
-            if best == n: return ans
+        array,ans = [0]*(n+1),0
+        for i in range(n+1):array[max(0,i-ranges[i])] = max(array[max(0,i-ranges[i])], min(n,i+ranges[i]))
+        best,next = array[0],0
+        for i in range(n+1):
             if i>best: return -1
             if i==best:
                 ans+=1
-                next = max(next,maxrange[i])
+                next = max(next,array[i])
                 best = next
                 next = 0
-            else: next = max(next,maxrange[i])
+            else: next = max(next,array[i])
         return ans
-            
-
-
-                
-            
-        
