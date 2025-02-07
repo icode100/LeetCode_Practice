59a2510867cff5ba6a4defe2bf3992c9e2f38dc0
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-02-07 10:26:31 +0530

    Time: 72 ms (59.02%) | Memory: 63.1 MB (58.08%) - LeetSync

--- Striver_SDE_SHEET/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ Striver_SDE_SHEET/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -1,17 +1,17 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        colors = defaultdict(set)
+        colors = defaultdict(int)
         bookkeeping = defaultdict(int)
         ans = list()
         for ball,col in queries:
             if ball in bookkeeping:
-                colors[bookkeeping[ball]].remove(ball)
-                if len(colors[bookkeeping[ball]])==0: colors.pop(bookkeeping[ball])
+                colors[bookkeeping[ball]]-=1
+                if colors[bookkeeping[ball]]==0: colors.pop(bookkeeping[ball])
                 bookkeeping[ball] = col
-                colors[col].add(ball)
+                colors[col]+=1
             else:
                 bookkeeping[ball] = col
-                colors[col].add(ball) 
+                colors[col]+=1
             ans.append(len(colors))
         return ans
 
