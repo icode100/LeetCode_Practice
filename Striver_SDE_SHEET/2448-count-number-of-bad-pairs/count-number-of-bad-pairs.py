cfc36165328d8039dafe1808a8104ca96c417a0a
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-02-09 08:36:58 +0530

    Time: 78 ms (60.95%) | Memory: 38.7 MB (88.57%) - LeetSync

--- Striver_SDE_SHEET/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ Striver_SDE_SHEET/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,8 +1,10 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         hashmap = defaultdict(int)
-        ans = 0
         for i,num in enumerate(nums):
-            ans+=(i-hashmap[num-i])
             hashmap[num-i]+=1
-        return ans
+        ans = 0
+        for key,val in hashmap.items():
+            ans+=(((val)*(val-1))>>1)
+        return ((len(nums)*(len(nums)-1))>>1)-ans
+        
