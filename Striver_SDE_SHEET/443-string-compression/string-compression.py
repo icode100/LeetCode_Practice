c0c19a6778a8744bc80d3ec950ad76b9fc7b74f6
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-02-26 15:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.51%) - LeetSync

--- Striver_SDE_SHEET/443-string-compression/string-compression.py
+++ Striver_SDE_SHEET/443-string-compression/string-compression.py
@@ -1,27 +1,17 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        stack = list()
-        ans = list()
-        for i in chars:
-            if not stack or stack[-1]==i: stack.append(i)
-            elif i!=stack[-1]:
-                top = stack[-1]
-                count = 0
-                while stack:
-                    stack.pop()
-                    count+=1
-                ans.append(top)
-                if count!=1: 
-                    for c in str(count):ans.append(c)
-                stack.append(i)
-        top = stack[-1]
-        count = 0
-        while stack:
-            stack.pop()
-            count+=1
-        ans.append(top)
-        if count!=1: 
-            for c in str(count):ans.append(c)
-        print(ans)
-        for i in range(len(ans)): chars[i] = ans[i]
-        return len(ans)
+        counter = 0
+        updator = 0
+        length = 0
+        while counter<len(chars):
+            top = chars[counter]
+            count = 0
+            while counter<len(chars) and chars[counter]==top:
+                count+=1
+                counter+=1
+            temp = top+str(count) if count!=1 else top
+            for c in temp:
+                chars[updator] = c
+                updator+=1
+                length+=1
+        return length
