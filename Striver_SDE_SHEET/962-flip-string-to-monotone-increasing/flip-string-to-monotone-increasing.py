7a9f23b793dcfe43c7b44d4a8b33c29463e84131
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-06 21:17:36 +0530

    Time: 477 ms (9.66%) | Memory: 30.8 MB (9.89%) - LeetSync

--- Striver_SDE_SHEET/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ Striver_SDE_SHEET/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -1,17 +1,13 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        @cache
-        def recursion(i,hasone):
-            if i==len(s): return 0
-            if hasone:
-                if s[i]=='0':
-                    return 1+recursion(i+1,hasone)
-                else:
-                    return recursion(i+1,hasone)
+        dp = [[0 for __ in range(2)] for _ in range(len(s)+1)]
+        for i in range(len(s)-1,-1,-1):
+            if s[i]=='0':
+                dp[i][0] = 1+dp[i+1][0]
+                dp[i][1] = min(1+dp[i+1][0],dp[i+1][1])
             else:
-                if s[i]=='0':
-                    return min(1+recursion(i+1,True),recursion(i+1,False))
-                else:
-                    return min(1+recursion(i+1,False),recursion(i+1,True))
+                dp[i][0] = dp[i+1][0]
+                dp[i][1] = min(1+dp[i+1][1],dp[i+1][0])
+        return min(dp[0])
+
         
-        return recursion(0,False)
