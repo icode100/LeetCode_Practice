1f100cd67d8644d16bde640975853c5ee70576b3
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-06 20:33:09 +0530

    Time: 3 ms (77.54%) | Memory: 18.1 MB (32.29%) - LeetSync

--- Striver_SDE_SHEET/1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ Striver_SDE_SHEET/1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -2,13 +2,15 @@ class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         days.sort()
         numday = [1,7,30]
-        dp = [inf for _ in range(len(days)+1)]
-        for i in range(len(days)-1,-1,-1):
+        @cache
+        def recursion(i):
+            if i>=len(days): return inf
+            ans = inf
             for j,c in enumerate(costs):
                 find = days[i]+numday[j]
                 idx = bisect_left(days,find)
-                # print(idx)
-                if idx >= len(days): dp[i] = min(dp[i],c)
-                else: dp[i] = min(dp[i], c+dp[idx])
-        return dp[0]
+                if idx==len(days): ans = min(ans,c)
+                else: ans = min(ans,c+recursion(idx))
+            return ans
+        return recursion(0)
                 
