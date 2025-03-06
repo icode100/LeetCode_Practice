0237964c73aad19d983ad2264e2235d500ad221d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-06 21:19:17 +0530

    Time: 706 ms (8.50%) | Memory: 56.3 MB (7.63%) - LeetSync

--- Striver_SDE_SHEET/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ Striver_SDE_SHEET/962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -1,6 +1,6 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        dp = [[0 for __ in range(2)] for _ in range(len(s)+1)]
+        dp = defaultdict(lambda:defaultdict(int))
         for i in range(len(s)-1,-1,-1):
             if s[i]=='0':
                 dp[i][0] = 1+dp[i+1][0]
@@ -8,6 +8,6 @@ class Solution:
             else:
                 dp[i][0] = dp[i+1][0]
                 dp[i][1] = min(1+dp[i+1][1],dp[i+1][0])
-        return min(dp[0])
+        return min(dp[0][0],dp[0][1])
 
         
