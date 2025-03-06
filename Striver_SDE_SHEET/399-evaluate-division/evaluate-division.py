349a409e2cfe7e64e022a9faaa19181c4230cf93
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-06 09:05:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.80%) - LeetSync

--- Striver_SDE_SHEET/399-evaluate-division/evaluate-division.py
+++ Striver_SDE_SHEET/399-evaluate-division/evaluate-division.py
@@ -6,11 +6,11 @@ class Solution:
             graph[u].append((v,w))
             graph[v].append((u,1/w))
         def dfs(node,dest,current,vis):
-            if node==dest: return current
+            if node==dest: return (True,current)
             vis.add(node)
             for ngh,weight in graph[node]:
                 if ngh not in vis:
-                    ans = dfs(ngh,dest,current*weight,vis)
-                    if ans!=-1: return ans
-            return -1
-        return [dfs(u,v,1,set()) if u in graph and v in graph else -1 for u,v in queries]
+                    check,ans = dfs(ngh,dest,current*weight,vis)
+                    if check: return (check,ans)
+            return (False,-1)
+        return [dfs(u,v,1,set())[1] if u in graph and v in graph else -1 for u,v in queries]
