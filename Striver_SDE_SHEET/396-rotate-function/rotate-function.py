86630e200e3e04780c7eda8ce37ef0ac6fa1817a
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-07 09:50:44 +0530

    Time: 183 ms (41.05%) | Memory: 25.8 MB (50.32%) - LeetSync

--- Striver_SDE_SHEET/396-rotate-function/rotate-function.py
+++ Striver_SDE_SHEET/396-rotate-function/rotate-function.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
         n = len(nums)
-        current = sum([i*nums[i] for i in range(n)])
-        inc = sum(nums)
+        current = 0
+        inc = 0
+        for i,num in enumerate(nums):
+            current+=(i*num)
+            inc+=num
         ans = current
         for k in range(1,n):
             current = current+inc-(nums[-k]*n)
