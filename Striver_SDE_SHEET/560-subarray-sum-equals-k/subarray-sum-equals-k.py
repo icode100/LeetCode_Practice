021f26d45e5a4b8cfdc2b814cf73339d06e12f85
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-11 08:59:43 +0530

    Time: 32 ms (53.08%) | Memory: 20.5 MB (36.35%) - LeetSync

---
+++ Striver_SDE_SHEET/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        hashmap,ans,current = {},0,0
+        for n in nums:
+            current+=n
+            if current==k: ans+=1
+            ans+=hashmap.get(current-k,0)
+            hashmap[current] = hashmap.get(current,0)+1
+        return ans
