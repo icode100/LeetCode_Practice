cd111dea3ba730aab0ea99851a4ce4a2f6bf5c29
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-11 11:24:51 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (29.15%) - LeetSync

--- Striver_SDE_SHEET/2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.py
+++ Striver_SDE_SHEET/2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.py
@@ -1,8 +1,3 @@
 class Solution:
     def twoEggDrop(self, n: int) -> int:
-        @cache
-        def recursion(e,f):
-            if e==1: return f
-            if f==0 or f==1: return f
-            return min([1+max(recursion(e-1,j-1),recursion(e,f-j)) for j in range(1,f+1)])
-        return recursion(2,n)
+        return math.ceil((-1+sqrt(1+8*n))/2)
