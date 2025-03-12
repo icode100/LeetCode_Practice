bb87539dab65605a5891978cd71f22741b5d376e
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-12 20:54:33 +0530

    Time: 303 ms (5.90%) | Memory: 52.1 MB (5.40%) - LeetSync

--- Striver_SDE_SHEET/2636-maximum-subsequence-score/maximum-subsequence-score.py
+++ Striver_SDE_SHEET/2636-maximum-subsequence-score/maximum-subsequence-score.py
@@ -1,21 +1,13 @@
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        n:int = len(nums1)
-        pairs:list[tuple[int,int]]  = list(zip(nums1,nums2))
-        pairs.sort(key = lambda x:(-x[1],-x[0]))
-        sumi:int = 0
-        heap:list[int] = list()
-        for i in range(k):
-            heapq.heappush(heap,pairs[i][0])
-            sumi+=pairs[i][0]
-        answer:int = sumi*pairs[k-1][1]
-
-        for i in range(k,n):
-            sumi-=heapq.heappop(heap)
-            sumi+=pairs[i][0]
-            heapq.heappush(heap,pairs[i][0])
-            answer = max(answer,sumi*pairs[i][1])
-        return answer
-
+        n = len(nums1)
+        pairs = sorted(zip(nums1,nums2), key = lambda x:(-x[1],-x[0]))
+        heap,sumi,ans = list(),0,0
+        for num1,num2 in pairs:
+            sumi+=num1
+            heappush(heap,num1)
+            if len(heap)>k: sumi-=heappop(heap)
+            if len(heap)==k:ans = max(ans,sumi*num2)
+        return ans
 
         
