7a230b78df6793ee3add4d7ba460197675a998c6
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-14 16:25:16 +0530

    Time: 63 ms (14.10%) | Memory: 22 MB (85.58%) - LeetSync

---
+++ Striver_SDE_SHEET/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def recursion(root):
+            if not root: return None
+            if root==p or root==q: return root
+            left = recursion(root.left)
+            right = recursion(root.right)
+            if left and right: return root
+            if not left: return right
+            if not right: return left
+            if not left and not right: return None
+        return recursion(root)
