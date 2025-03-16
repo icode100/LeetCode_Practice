08ca04e23495b086e504c29e79db48495d3fd733
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-16 21:19:33 +0530

    Time: 155 ms (72.70%) | Memory: 63.7 MB (20.20%) - LeetSync

--- Striver_SDE_SHEET/3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ Striver_SDE_SHEET/3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
         n = len(nums)
-        def check(mid):
-            dp = [0]*n
-            current = 0
-            for s,e,v in queries[:mid]:
-                dp[s]+=v
-                if e+1<n: dp[e+1]-=v
-            for i,num in enumerate(dp):
-                current+=num
-                if current<nums[i]: return False
-            return True
-        left,right = 0,len(queries)
-        while left<=right:
-            mid = (left+right)//2
-            if check(mid): right = mid-1
-            else: left = mid+1
-        return left if left<=len(queries) else -1
+        dp = [0]*n
+        current=0
+        ans = 0
+        for i in range(n):
+            while current+dp[i]<nums[i]:
+                ans+=1
+                if ans>len(queries): return -1
+                s,e,v = queries[ans-1]
+                if e>=i:
+                    dp[max(i,s)]+=v
+                    if e+1<n: dp[e+1]-=v
+            current+=dp[i]
+        return ans
+            
+
 
