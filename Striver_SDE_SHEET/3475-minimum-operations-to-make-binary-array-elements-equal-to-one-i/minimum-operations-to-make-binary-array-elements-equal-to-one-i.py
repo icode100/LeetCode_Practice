24d6aaf6e86a9404671086e58e80a5a03430ace7
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-20 19:51:27 +0530

    Time: 164 ms (29.76%) | Memory: 21.6 MB (55.09%) - LeetSync

--- Striver_SDE_SHEET/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ Striver_SDE_SHEET/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,14 +1,21 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         n = len(nums)
-        count=0
-        l = 0
-        for r in range(n):
-            if nums[r]==0:
-                if r+2>=n: return -1
-                for j in range(r,r+3):
-                    nums[j] = 1-nums[j]
-                count+=1
+        k=3
+        q = deque()
+        count = 0
+        for i in range(n):
+            while q and q[0]<(i-k+1): q.popleft()
+            if nums[i]==0:
+                if len(q)&1: continue
+                else: 
+                    if i+k>n: return -1
+                    count+=1
+                    q.append(i)
+            if nums[i]==1:
+                if len(q)&1:
+                    if i+k>n: return -1
+                    count+=1 
+                    q.append(i)
+                else: continue
         return count
-
-            
