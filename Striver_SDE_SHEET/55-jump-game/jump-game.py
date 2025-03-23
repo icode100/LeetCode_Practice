68961961b90deed95b1fab723a008dc484935139
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-23 13:32:08 +0530

    Time: 47 ms (24.12%) | Memory: 18.5 MB (86.82%) - LeetSync

--- Striver_SDE_SHEET/55-jump-game/jump-game.py
+++ Striver_SDE_SHEET/55-jump-game/jump-game.py
@@ -1,11 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        current = 0
+        max_i = 0
         for i in range(n):
-            if i>current: return False
-            current = max(current,i+nums[i])
+            if i>max_i: return False
+            max_i = max(max_i,i+nums[i])
         return True
+                
 
 
                 
