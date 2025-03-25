ce8d1c3366518e703a9a281af790d7e7ba0bd2f9
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-25 16:50:16 +0530

    Time: 39 ms (38.95%) | Memory: 19.1 MB (75.01%) - LeetSync

--- Striver_SDE_SHEET/876-hand-of-straights/hand-of-straights.py
+++ Striver_SDE_SHEET/876-hand-of-straights/hand-of-straights.py
@@ -2,14 +2,14 @@ class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         hand.sort()
         counter = Counter(hand)
-        ends = defaultdict(int)
-        for i in hand:
-            if counter[i]==0: continue
-            for j in range(groupSize):
-                if counter[i+j]!=0:
-                    counter[i+j]-=1
-                else: return False
-        return True
+        for x in hand:
+            if x not in counter: continue
+            for diff in range(groupSize):
+                if x+diff not in counter: return False
+                counter[x+diff]-=1
+                if counter[x+diff]==0: counter.pop(x+diff)
+        return True 
+            
                 
         
 
