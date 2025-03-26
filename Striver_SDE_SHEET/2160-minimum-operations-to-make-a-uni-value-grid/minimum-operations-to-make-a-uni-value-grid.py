dc59c39326a0538aa5927a24a9a8b15d059549ff
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-26 23:57:41 +0530

    Time: 204 ms (32.89%) | Memory: 39.6 MB (19.30%) - LeetSync

--- Striver_SDE_SHEET/2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ Striver_SDE_SHEET/2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -2,17 +2,11 @@ class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         m,n = len(grid), len(grid[0])
         k = m*n 
-        checkset = set()
-        sumi = 0
+        nums = sorted([grid[i][j] for i in range(m) for j in range(n)])
+        num = nums[k>>1]
+        ans = 0
         for i in range(m):
             for j in range(n):
-                checkset.add(grid[i][j]%x)
-                sumi+=grid[i][j]
-        if len(checkset)>1: return -1
-        nums = sorted(grid[i][j] for i in range(m) for j in range(n))
-        ans = inf
-        current = 0
-        for i in range(k):
-            ans = min(ans,(nums[i]*i - current + sumi-current-(nums[i]*(k-i)) )//x)
-            current+=nums[i]
-        return max(ans,0)
+                if abs(grid[i][j]-num)%x!=0: return -1
+                ans+=abs(grid[i][j]-num)//x
+        return ans
