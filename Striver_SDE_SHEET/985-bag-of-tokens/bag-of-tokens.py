7aa8d009050b69bf46d9773a2146fb237ca1b72d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-26 21:55:48 +0530

    Time: 12 ms (5.56%) | Memory: 18.3 MB (19.53%) - LeetSync

--- Striver_SDE_SHEET/985-bag-of-tokens/bag-of-tokens.py
+++ Striver_SDE_SHEET/985-bag-of-tokens/bag-of-tokens.py
@@ -1,27 +1,27 @@
-from collections import deque
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
-        if not tokens:
-            return 0
-        max_score = 0
-        q = deque(sorted(tokens))
-        if power<q[0]:
-            return 0
-        faceup = True
+        tokens.sort()
+        n = len(tokens)
         score = 0
-        while q:
-            if faceup:
+        l,r = 0,n-1
+        ans = 0
+        if not tokens or power<tokens[0]: return 0
+        while l<r:
+            while l<=r and tokens[l]<=power:
                 score+=1
-                max_score = max(score,max_score)
-                power-=q.popleft()
-                if q:
-                    faceup = False if power<q[0] else True
-            else:
+                power-=tokens[l]
+                l+=1
+                ans = max(score,ans)
+                print(l,r)
+            while l<=r and score>=1 and tokens[l]>power:
                 score-=1
-                max_score = max(score,max_score)
-                power+=q.pop()
-                if q:
-                    faceup = False if power<q[0] else True
-        return max_score
+                power+=tokens[r]
+                r-=1
+                print(l,r)
 
-
+        if l==r and l<n:
+            if tokens[l]<=power:
+                score+=1
+                ans = max(ans,score)
+        ans = max(ans,score)
+        return ans
