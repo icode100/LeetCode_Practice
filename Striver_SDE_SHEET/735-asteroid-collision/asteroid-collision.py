5f3c2481a05f930dbe439754cf20d869a685f85e
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-27 19:44:51 +0530

    Time: 7 ms (58.64%) | Memory: 18.7 MB (96.66%) - LeetSync

---
+++ Striver_SDE_SHEET/735-asteroid-collision/asteroid-collision.py
@@ -0,0 +1,14 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = list()
+        for i in asteroids:
+            while stack and i<0 and stack[-1]>0 and abs(stack[-1])<abs(i):
+                stack.pop()
+            if stack and i<0 and stack[-1]>0 :
+                if abs(stack[-1])==abs(i):
+                    stack.pop()
+                continue
+                
+            # if stack and i*stack[-1]<0 and abs(stack[-1])>abs(i): continue
+            stack.append(i)
+        return stack
