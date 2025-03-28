933579f902ac9d16c69e1d2547d1b2afa754e73a
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-03-28 18:34:58 +0530

    Time: 26 ms (5.44%) | Memory: 22 MB (15.00%) - LeetSync

--- Striver_SDE_SHEET/56-merge-intervals/merge-intervals.py
+++ Striver_SDE_SHEET/56-merge-intervals/merge-intervals.py
@@ -1,9 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         ans = list()
-        intervals.sort()
+        d = SortedDict()
         for s,e in intervals:
-            if not ans or s>ans[-1][1]: ans.append([s,e])
-            else:
-                ans[-1][1] = max(ans[-1][1],e)
+            d[s] = d.get(s,0)+1
+            d[e] = d.get(e,0)-1
+        start = 0
+        count = 0
+        for k,v in d.items():
+            if count==0: start = k
+            count+=v
+            if count == 0: ans.append([start,k])
         return ans
+
