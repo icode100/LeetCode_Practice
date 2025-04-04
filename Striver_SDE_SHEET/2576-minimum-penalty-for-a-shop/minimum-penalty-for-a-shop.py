7e115401867b00ba0f40f0fd865a39b94f8528b6
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-04 10:22:33 +0530

    Time: 194 ms (13.18%) | Memory: 18.2 MB (63.18%) - LeetSync

--- Striver_SDE_SHEET/2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ Striver_SDE_SHEET/2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,19 +1,11 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        max_score = score = 0
-        best_hour = -1
-
-        for i, c in enumerate(customers):
-            score += 1 if c == 'Y' else -1
-            if score > max_score:
-                max_score, best_hour = score, i
-                
-        return best_hour + 1
-
-
-
-
-
-
-            
-        
+        N = len(customers)
+        counter = Counter(customers)
+        penalty = N
+        yes,no,ans = 0,0,0
+        for i in range(N+1):
+            if no+counter['Y']-yes<penalty: ans,penalty = i,no+counter['Y']-yes
+            if i<N: yes,no = yes+int(customers[i]=='Y'), no+int(customers[i]=='N')
+        return ans
+ 
