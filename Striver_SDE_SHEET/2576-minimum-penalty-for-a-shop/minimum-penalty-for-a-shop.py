acf338ce26fdacb003d584490fdd1efaf57a672c
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-04 10:27:17 +0530

    Time: 146 ms (27.53%) | Memory: 18.3 MB (44.59%) - LeetSync

--- Striver_SDE_SHEET/2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ Striver_SDE_SHEET/2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,11 +1,11 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         N = len(customers)
-        counter = Counter(customers)
+        counter = customers.count("Y")
         penalty = N
         yes,no,ans = 0,0,0
         for i in range(N+1):
-            if no+counter['Y']-yes<penalty: ans,penalty = i,no+counter['Y']-yes
+            if no+counter-yes<penalty: ans,penalty = i,no+counter-yes
             if i<N: yes,no = yes+int(customers[i]=='Y'), no+int(customers[i]=='N')
         return ans
  
