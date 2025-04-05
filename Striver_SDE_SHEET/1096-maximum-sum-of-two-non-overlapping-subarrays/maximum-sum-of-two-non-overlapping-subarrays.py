48f8d91dc60b6fa1ba009b038e48234301f0dd73
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-05 13:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (54.40%) - LeetSync

--- Striver_SDE_SHEET/1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
+++ Striver_SDE_SHEET/1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.py
@@ -2,22 +2,29 @@
 class Solution:
     def maxSumTwoNoOverlap(self, A: List[int], L: int, M: int) -> int:
 
+# * two cases: L occurs first then M (OR) M occurs first then L handle them separately
+# * prefix = accummulate(nums) // prefix array
+# * first option for L => nums[0:L] thus max_lsum = prefix[L-1]; first option for M max_msum = prefix[M-1]
+# * AIM: Maximizing lsum and msum hence maximizing lsum+msum
+# * case 1: L occurs before M
+#     * start iteration from L+M so that we dont get out of bound
+#     * current msum = prefix[i]-prefix[i-M]
+#     * current lsum = prefix[i-M]-prefix[i-M-L]
+#     * max_lsum = max(max_lsum,lsum)
+#     * ans = max(ans, msum+max_lsum)
+# * case 2: M occurs before L
+#     * do the same try maximizing msum and then maximize current lsum + max_msum
+# return ans
 
-        # do a CDF so that range sum can easily be calculated
-        for i in range(1, len(A)):
-            A[i] += A[i - 1]
-
-
-        res, Lmax, Mmax = A[L + M - 1], A[L - 1], A[M - 1]
-
-
-        # window  | --- L --- | --- M --- |
-        for i in range(L + M, len(A)):
-            Lmax = max(Lmax, A[i - M] - A[i - L - M])
-            res = max(res, Lmax + A[i] - A[i - M])
-
-        # window  | --- M --- | --- L --- |
-            Mmax = max(Mmax, A[i - L] - A[i - L - M])
-            res = max(res, Mmax + A[i] - A[i - L])
-
-        return res
+        N = len(A)
+        prefix = list(accumulate(A))
+        max_lsum,max_msum = prefix[L-1],prefix[M-1]
+        ans = prefix[L+M-1]
+        for i in range(L+M,N):
+            msum = prefix[i]-prefix[i-M]
+            lsum = prefix[i]-prefix[i-L]
+            max_lsum = max(max_lsum, prefix[i-M]-prefix[i-M-L])
+            max_msum = max(max_msum, prefix[i-L]-prefix[i-L-M])
+            ans = max(ans, max_lsum + msum, max_msum + lsum)
+        return ans
+            
