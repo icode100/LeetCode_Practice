ca7ab47495ea903107ec8030d310059268725b81
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-06 13:42:22 +0530

    Time: 208 ms (27.43%) | Memory: 18.3 MB (22.26%) - LeetSync

--- Striver_SDE_SHEET/368-largest-divisible-subset/largest-divisible-subset.py
+++ Striver_SDE_SHEET/368-largest-divisible-subset/largest-divisible-subset.py
@@ -2,13 +2,31 @@ class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
         N = len(nums)
-        @cache
-        def recursion(i,prev):
-            if i>=N: return []
-            notpick = recursion(i+1,prev)
-            pick = []
-            if nums[i]%prev==0:
-                pick = [nums[i]]+recursion(i+1,nums[i])
-            return pick if len(pick)>len(notpick) else notpick
-        return recursion(0,1)
+
+        dp = [deque([i]) for i in nums]
+        ans = list()
+        for i in range(N):
+            for j in range(i):
+                if nums[i]%nums[j]==0:
+                    dp[j].append(nums[i])
+                    temp = dp[j].copy()
+                    dp[j].pop()
+                    if len(temp)>len(dp[i]): dp[i] = temp
+                    
+            ans = dp[i] if len(dp[i])>len(ans) else ans
+        return list(ans)
+        # @cache
+        # def recursion(i):
+        #     if i>=N: return []
+        #     ans = [nums[i]]
+        #     for j in range(i+1,N):
+        #         if nums[j]%nums[i]==0:
+        #             temp = [nums[i]]+recursion(j)
+        #             ans = temp if len(temp)>len(ans) else ans
+        #     return ans
+        # ans = list()
+        # for i in range(N):
+        #     temp = recursion(i)
+        #     ans = temp if len(temp)>len(ans) else ans
+        # return ans
             
