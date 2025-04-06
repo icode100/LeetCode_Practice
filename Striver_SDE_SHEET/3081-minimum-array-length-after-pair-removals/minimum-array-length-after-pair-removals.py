696b73bfd1424f084a61cf6fce0483edafd8645c
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-06 18:57:05 +0530

    Time: 56 ms (57.26%) | Memory: 32.5 MB (47.69%) - LeetSync

--- Striver_SDE_SHEET/3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.py
+++ Striver_SDE_SHEET/3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.py
@@ -1,16 +1,9 @@
 class Solution:
     def minLengthAfterRemovals(self, nums: List[int]) -> int:
-        n = len(nums)
-        l = 0
-        median = ceil(len(nums)/2)
-        r = median
-        removed = set()
-        while l<r and r<n:
-            if nums[l]<nums[r] and l not in removed:
-                removed.add(l)
-                removed.add(r)
-                l+=1
-            r+=1
-        return len(nums)-len(removed)
+        N = len(nums)
+        x = max(Counter(nums).values())
+        if x>(N>>1): return x-(N-x)
+        return N&1
+        
 
                 
