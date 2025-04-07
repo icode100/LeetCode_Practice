4bbca749a3f6a3b1dc254c6f509bee5c1391957d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-07 13:28:44 +0530

    Time: 231 ms (91.35%) | Memory: 19.6 MB (55.27%) - LeetSync

--- Striver_SDE_SHEET/416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ Striver_SDE_SHEET/416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,19 +4,14 @@ class Solution:
         total = sum(nums)
         if total&1: return False
         target = total//2
-        dp = [[False for _ in range(total)] for _ in range(N+1)]
-        for i in range(N+1): dp[i][target]=True
+        dp = {0}
         for i in range(N-1,-1,-1):
-            for current in range(target):
-                dp[i][current] = dp[i+1][current] or (dp[i+1][current+nums[i]] if current+nums[i]<=target else False)
-        return dp[0][0]
-
-
-        # @cache
-        # def recursion(index, current):
-        #     if current==(total//2): return True
-        #     if index==N: return current==(total//2)
-        #     notpick = recursion(index+1,current)
-        #     pick = recursion(index+1,current+nums[index]) if nums[index]+current<=(total//2) else False
-        #     return pick or notpick
-        # return recursion(0,0)
+            temp = set()
+            for d in dp:
+                if d==target: return True
+                temp.add(d+nums[i])
+                temp.add(d)
+            dp = temp
+        return False
+            
+                
