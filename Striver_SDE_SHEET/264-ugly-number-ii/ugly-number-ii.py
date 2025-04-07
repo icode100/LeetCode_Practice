d5206dd7d688a04be5304773154103e44e3f23e4
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-07 20:11:12 +0530

    Time: 91 ms (29.08%) | Memory: 18.1 MB (18.13%) - LeetSync

--- Striver_SDE_SHEET/264-ugly-number-ii/ugly-number-ii.py
+++ Striver_SDE_SHEET/264-ugly-number-ii/ugly-number-ii.py
@@ -1,13 +1,14 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        heap = SortedSet([1])
+        heap = [1]
         ans = 0
+        vis = {1}
         while n:
-            ans = heap[0]
-            heap.remove(ans)
+            ans = heappop(heap)
             n-=1
             if not n: return ans
             for i in [2,3,5]:
-                heap.add(ans*i)
-            
+                if ans*i not in vis: 
+                    heappush(heap,ans*i)
+                    vis.add(ans*i)
         return ans
