710f59348b70b8e4cf15dab785f1cc8f16c60660
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-08 21:39:48 +0530

    Time: 366 ms (43.60%) | Memory: 32.3 MB (13.98%) - LeetSync

--- Striver_SDE_SHEET/2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.py
+++ Striver_SDE_SHEET/2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.py
@@ -1,18 +1,17 @@
 class Solution:
     def countDistinct(self, nums: List[int], k: int, p: int) -> int:
-        cnt = 0
-        n = len(nums)
-        start = 0
-        hashing = set()
-        for end in range(n):
-            if nums[end]%p==0:
-                cnt+=1
-            while cnt>k and start<end:
-                for i in range(start+1,end+1):hashing.add(tuple(nums[start:i]))
-                if not nums[start]%p: cnt-=1
-                start+=1
-        end+=1
-        while start<end:
-            for i in range(start+1,end+1):hashing.add(tuple(nums[start:i]))
-            start+=1
-        return len(hashing)
+        N = len(nums)
+        ans = 0
+        count = 0
+        l = 0
+        hashset = set()
+        for r in range(N):
+            count+=int((nums[r]%p)==0)
+            while l<r and count>k:
+                count-=int((nums[l]%p)==0)
+                l+=1
+            string = ""
+            for i in range(l,r+1):
+                hashset.add(tuple(nums[i:r+1]))
+        return len(hashset)
+
