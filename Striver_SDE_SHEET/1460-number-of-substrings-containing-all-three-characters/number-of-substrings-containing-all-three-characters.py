428e694c4462e463f03b08086144b5c9ecaec61c
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-08 14:04:44 +0530

    Time: 147 ms (29.69%) | Memory: 17.9 MB (68.19%) - LeetSync

--- Striver_SDE_SHEET/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ Striver_SDE_SHEET/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,12 +1,15 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        n = len(s)
-        hashmap,ans,l = {},0,0
-        for r in range(n):
-            hashmap[s[r]] = hashmap.get(s[r],0)+1
-            while l<r and len(hashmap)==3:
-                ans+=n-r
-                hashmap[s[l]]-=1
-                if hashmap[s[l]]==0: hashmap.pop(s[l])
+        N = len(s)
+        ans = 0
+        l=0
+        checkset = Counter()
+        for r in range(N):
+            checkset[s[r]]+=1
+            while l<r and len(checkset)==3:
+                ans+=(N-r)
+                checkset[s[l]]-=1
+                if checkset[s[l]]==0: checkset.pop(s[l])
                 l+=1
         return ans
+
