a24a5ed068e0aeec20debb9419bae3f3cc66990d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-09 06:48:02 +0530

    Time: 111 ms (21.21%) | Memory: 28.4 MB (67.58%) - LeetSync

--- Striver_SDE_SHEET/11-container-with-most-water/container-with-most-water.py
+++ Striver_SDE_SHEET/11-container-with-most-water/container-with-most-water.py
@@ -1,15 +1,10 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        n = len(height)
-        l,r = 0,n-1
-        function =lambda l,r: (r-l)*min(height[l],height[r])
-        ans = function(l,r)
+        N = len(height)
+        l,r = 0,N-1
+        ans = 0
         while l<=r:
-            if height[l]<height[r]:
-                l+=1
-            else:
-                r-=1
-            ans = max(ans,function(l,r))
+            ans = max(ans,(r-l)*min(height[r],height[l]))
+            if height[l]<height[r]: l+=1
+            else: r-=1
         return ans
-            
