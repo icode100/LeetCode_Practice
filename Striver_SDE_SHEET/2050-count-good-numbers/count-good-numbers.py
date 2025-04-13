6656bc153cd692548174f70c179eb8300099ef85
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-13 19:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (5.31%) - LeetSync

--- Striver_SDE_SHEET/2050-count-good-numbers/count-good-numbers.py
+++ Striver_SDE_SHEET/2050-count-good-numbers/count-good-numbers.py
@@ -1,6 +1,6 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        even = (n>>1)+1 if n&1 else (n>>1)
-        odd = n-even
         mod = int(1e9+7)
+        even = ceil(n/2)
+        odd = n-even
         return (((pow(5,even,mod))%mod )*(pow(4,odd, mod))) % mod
