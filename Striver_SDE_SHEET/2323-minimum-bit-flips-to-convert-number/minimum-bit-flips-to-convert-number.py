604082b88c06a12acca96c7c10d37c743f5bddfd
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-14 10:20:06 +0530

    Time: 1 ms (11.81%) | Memory: 17.9 MB (12.82%) - LeetSync

---
+++ Striver_SDE_SHEET/2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        ans = start^goal
+        count = 0
+        while ans:
+            count+=(ans&1)
+            ans = (ans>>1)
+        return count
