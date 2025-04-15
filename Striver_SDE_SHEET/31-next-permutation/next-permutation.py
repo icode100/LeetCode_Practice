a1bfbb0e1c9a8e9a8357be0b89449bbd199cb506
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-04-15 10:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (14.61%) - LeetSync

--- Striver_SDE_SHEET/31-next-permutation/next-permutation.py
+++ Striver_SDE_SHEET/31-next-permutation/next-permutation.py
@@ -1,16 +1,28 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        index = -1
-        for i in range(len(nums)-2,-1,-1):
-            if nums[i]<nums[i+1]:
-                index = i
+        '''
+        current => 1 2 4 6 5 3
+        next    => 1 2 5 3 4 6 
+
+        observations: 
+        * 4 is the pivotal point prefix till 4 remains the same 
+        * because idx = index(4) is the first index starting from the end of array where nums[idx]<nums[idx+1]
+        * now 4 should be swapped with the absolute next greater element in the suffix from index(4).
+        * making 1 2 5 6 4 3
+        * now we arrange the suffix in ascending order making it [1 2 5 3 6 4]
+        '''
+        N = len(nums)
+        idx = -1
+        for i in range(N-2,-1,-1):
+            if nums[i]<nums[i+1]: 
+                idx = i
                 break
-        if index==-1: 
-           nums.reverse()
-           return 
-        for i in range(len(nums)-1,index-1,-1):
-            if nums[index]<nums[i]:
-                nums[index],nums[i] = nums[i],nums[index]
+        if idx==-1: 
+            nums[::1] = nums[::-1]
+            return 
+        for i in range(N-1,idx,-1):
+            if nums[i]>nums[idx]:
+                nums[i],nums[idx] = nums[idx], nums[i]
                 break
-        nums[::1] = nums[:index+1]+nums[index+1:][::-1]
-        
+        print(idx, nums[:idx+1])
+        nums[::1] = nums[:idx+1]+sorted(nums[idx+1:])
