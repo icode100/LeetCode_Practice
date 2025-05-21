2f52bc056c2bdc7f09e16e4c3b2b3f92bf491c31
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-05-21 08:24:23 +0530

    Time: 261 ms (8.31%) | Memory: 53.3 MB (54.31%) - LeetSync

--- Striver_SDE_SHEET/1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ Striver_SDE_SHEET/1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
+        N = len(events)
         events.sort()
-        k = 0
-        ans,heap = 0,list()
-        for i in range(1,int(1e5)+1):
-            while heap and heap[0]<i: heappop(heap)
-            while k<len(events) and events[k][0]<=i: 
-                heappush(heap,events[k][1])
-                k+=1
-            if heap and heap[0]>=i:
-                ans+=1
+        heap = list()
+        i,count = 0,0
+        for d in range(1,int(1e5)+1):
+            while heap and heap[0]<d: heappop(heap)
+            while i<N and events[i][0]<=d: 
+                heappush(heap,events[i][1])
+                i+=1
+            if heap and heap[0]>=d: 
+                count+=1
                 heappop(heap)
-        return ans
-        
+        return count
