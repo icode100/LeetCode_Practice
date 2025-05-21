6f7ed0823f042989b2facb77243717a1fed755c3
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-05-21 09:54:36 +0530

    Time: 106 ms (27.51%) | Memory: 45.2 MB (28.38%) - LeetSync

--- Striver_SDE_SHEET/1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
+++ Striver_SDE_SHEET/1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
@@ -1,15 +1,11 @@
 class Solution:
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
-        n = len(nums)
-        hashmap = [0]*(n+1)
-        for i,j in requests:
-            hashmap[i]+=1
-            hashmap[j+1]-=1
-        for i in range(1,len(hashmap)): hashmap[i]+=hashmap[i-1]
-        hashmap = hashmap[:-1]
-        trav = list(zip(sorted(hashmap),sorted(nums)))
-        ans = 0
-        for i in range(len(trav)):
-            f,n = trav[i]
-            ans += (f*n)
-        return ans%int(1e9+7)
+        N = len(nums)
+        counter = [0]*N
+        for start,end in requests:
+            counter[start] +=1
+            if end+1<N: counter[end+1] -=1
+        for i in range(1,N): counter[i]+=counter[i-1]
+        return sum(n*c for n,c in list(zip(sorted(counter),sorted(nums)))) % int(1e9+7)
+
+        
