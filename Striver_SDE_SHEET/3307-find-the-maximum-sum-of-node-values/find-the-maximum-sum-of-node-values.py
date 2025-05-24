a6c2efc3c16e8edc7ad3de4e40a050cb8e85b6fb
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-05-24 09:24:26 +0530

    Time: 65 ms (33.33%) | Memory: 27.7 MB (17.54%) - LeetSync

--- Striver_SDE_SHEET/3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
+++ Striver_SDE_SHEET/3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -1,23 +1,33 @@
-INT_MAX = sys.maxsize
-INT_MIN = -INT_MAX-1
-class Solution:
-    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        '''
-        * the tree is undirected so we can use stone transfer 
-        * so practically there is no significance of the edge right because its a tree so no cycles and undirected so we can choose any two nodes
-        * except the number of operations should be even as at a single point we update two nodes and not one
-        * so now it boils down to try all possible ways so DP
-        '''
-        N = len(nums)
-        @cache
-        def recursion(index,parity):
-            if index==N:
-                if parity: return 0
-                else: return INT_MIN
-            pick = (nums[index]^k)+recursion(index+1,not parity)
-            notpick = nums[index]+recursion(index+1,parity)
-            return max(pick,notpick)
-        return recursion(0,True)
-
-
-        
+class Solution(object):
+    def maximumValueSum(self, nums, k, edges):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        n = len(nums)
+        after = []
+        for i in range(n):
+            t = nums[i] ^ k
+            after.append([t - nums[i], nums[i], t])
+        after.sort(key = lambda x:x[0])
+        def summ(array, ind):
+            tot = 0
+            for lists in array:
+                tot += lists[ind]
+            return tot
+        if after[-1][0] < 0:
+            return summ(after, 1)
+        if after[0][0] >= 0:
+            if n % 2 == 0:
+                return summ(after, 2)
+            return summ(after[1:], 2) + summ(after[:1], 1)
+        l = bisect.bisect_left(after, [0, -1, -1])
+        if after[l][0] < 0:
+            l += 1
+        if (n - l) % 2 == 0:
+            return summ(after[l:], 2) + summ(after[:l], 1)
+        if after[l - 1][0] + after[l][0] > 0:
+            return summ(after[l - 1:], 2) + summ(after[:l - 1], 1)
+        return summ(after[l + 1:], 2) + summ(after[:l + 1], 1)
