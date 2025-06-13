7efc1860923e48003ed5f02c2dc6df31e5dd8d13
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-06-13 19:02:32 +0530

    Time: 945 ms (72.12%) | Memory: 17.8 MB (88.79%) - LeetSync

--- Striver_SDE_SHEET/2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.py
+++ Striver_SDE_SHEET/2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.py
@@ -2,16 +2,17 @@ class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
         N = len(nums)
         nums.sort()
+        
         def dfs(index,vis):
-            if index>=N: return 0 if not vis else 1
+            if index>=N: return 0 if vis==0 else 1
             # notpick
             notpick = dfs(index+1,vis)
             # pick
             pick = 0
-            if nums[index]-k not in vis:
-               pick = dfs(index+1,vis|{nums[index]})
+            if ((nums[index]-k)>=0 and ((1<<(nums[index]-k)) & vis)==0) or ((nums[index]-k)<0):
+               pick = dfs(index+1,vis|(1<<(nums[index])))
             return pick+notpick
-        return dfs(0,set())
+        return dfs(0,0)
 
             
 
