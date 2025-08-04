2c6abf2ba2050363744132291d61cd90d7fa60f9
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-08-04 10:55:23 +0530

    Time: 177 ms (71.40%) | Memory: 23.6 MB (77.01%) - LeetSync

--- Striver_SDE_SHEET/940-fruit-into-baskets/fruit-into-baskets.py
+++ Striver_SDE_SHEET/940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,14 +1,27 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        # dont fixate the algorithm before hand
+        # each basket holds only one type of fruit
+        # pick one fruit from everytree one fruit cannot be picked stop
+        # have a mask for having the check only two kinds of fruits
+        # pick and not pick
+        # once we reach an invalid index we stop and also we should pick the fruit from every tree
+        # another catch is we can start from any index
+        # So its longest valid subarray
+        hashset = {}
         N = len(fruits)
-        checkset = defaultdict(int)
-        l = 0
         ans = 0
+        count = 0
+        l=0
         for r in range(N):
-            checkset[fruits[r]]+=1
-            while l<r and len(checkset)>2:
-                checkset[fruits[l]]-=1
-                if checkset[fruits[l]]==0: checkset.pop(fruits[l])
+            hashset[fruits[r]] = hashset.get(fruits[r],0)+1
+            count+=1
+            while len(hashset)>2:
+                hashset[fruits[l]]-=1
+                if hashset[fruits[l]]==0: hashset.pop(fruits[l])
+                count-=1
                 l+=1
-            ans = max(ans,r-l+1)
+            ans = max(count,ans)
         return ans
+
+        
