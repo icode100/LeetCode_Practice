ab15cc9c258de9c75ee48fa5e59a81a96628828d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-09-21 10:33:48 +0530

    Time: 95 ms (19.33%) | Memory: 32.8 MB (22.00%) - LeetSync

--- Striver_SDE_SHEET/53-maximum-subarray/maximum-subarray.py
+++ Striver_SDE_SHEET/53-maximum-subarray/maximum-subarray.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         minsum = 0
-        ans = -1e6
-        current = 0
-        for i in nums: 
-            current+=i
-            ans = max(ans,current-minsum)
-            minsum = min(minsum,current)
+        sum = 0
+        ans = -sys.maxsize-1
+        for num in nums:
+            sum+=num
+            ans = max(ans,sum-minsum)
+            minsum = min(minsum,sum)
         return ans
