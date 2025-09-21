c915fc93e43602b4df8635813fc7adfdeaea7c2c
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-09-21 09:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.96%) - LeetSync

--- Striver_SDE_SHEET/75-sort-colors/sort-colors.py
+++ Striver_SDE_SHEET/75-sort-colors/sort-colors.py
@@ -1,21 +1,21 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         N = len(nums)
-        l,r,e = 0,0,N-1
-        while r<=e:
-            if l>r:
-                r = l
+        zero,one,two = 0,0,N-1
+        while one<=two:
+            if zero>one: 
+                one = zero
                 continue
-            if nums[r]==0:
-                nums[l],nums[r] = nums[r],nums[l]
-                l+=1
+            if nums[one]==0:
+                nums[one],nums[zero] = nums[zero],nums[one]
+                zero+=1
                 continue
-            elif nums[r]==2:
-                nums[e],nums[r] = nums[r],nums[e]
-                e-=1
+            if nums[one]==2:
+                nums[one],nums[two] = nums[two], nums[one]
+                two-=1
                 continue
-            r+=1
-        return 
+            one+=1
 
 
+            
         
