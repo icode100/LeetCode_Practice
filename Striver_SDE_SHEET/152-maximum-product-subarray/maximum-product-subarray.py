c82e3cacc0ead8158b3ae9886dd731d5f48a3b2a
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-09-23 20:39:43 +0530

    Time: 9 ms (61.31%) | Memory: 18.4 MB (52.90%) - LeetSync

--- Striver_SDE_SHEET/152-maximum-product-subarray/maximum-product-subarray.py
+++ Striver_SDE_SHEET/152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        ans = -inf
-        current = 1
-        for i,num in enumerate(nums):
-            if current==0: current = 1
-            current*=num
-            ans = max(current,ans)
-        current = 1
-        for i,num in enumerate(nums[::-1]):
-            if current==0: current = 1
-            current*=num
-            ans = max(current,ans)
+        ans = -sys.maxsize-1
+        p = 1
+        for num in nums:
+            if p==0: p=1
+            p*=num
+            ans = max(ans,p)
+        p = 1
+        for num in nums[::-1]:
+            if p==0: p=1
+            p*=num
+            ans = max(ans,p)
         return ans
