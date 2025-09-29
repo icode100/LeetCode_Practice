ca3c47e122708287d10b68d203dbd1052dec8937
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-09-29 12:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.34%) - LeetSync

---
+++ Striver_SDE_SHEET/812-rotate-string/rotate-string.py
@@ -0,0 +1,6 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s)!=len(goal): return False
+        for i in range(len(s)):
+            if goal==s[i:]+s[:i]: return True
+        return False
