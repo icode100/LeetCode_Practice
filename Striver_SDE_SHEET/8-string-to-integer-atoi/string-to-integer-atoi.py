b53acd1b5f4900d00ae693f59397b0ad709af38e
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-09-30 11:18:26 +0530

    Time: 1 ms (53.33%) | Memory: 17.9 MB (48.59%) - LeetSync

--- Striver_SDE_SHEET/8-string-to-integer-atoi/string-to-integer-atoi.py
+++ Striver_SDE_SHEET/8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,16 +1,19 @@
+INT_MAX,INT_MIN = ((1<<31)-1), -(1<<31)
 class Solution:
     def myAtoi(self, s: str) -> int:
         s = s.strip()
-        print(s)
-        ans = '0'
+        if not s:return 0
         sign = 1
-        for i,c in enumerate(s):
-            if i==0 and c in '+-': 
-                sign = -1 if c=="-" else 1
-            elif c in '1234567890': ans+=c
-            else: 
-                print(c)
-                break
-        INT_MAX,INT_MIN = ((1<<31)-1), -(1<<31)
-        val = int(ans)*sign
-        return val if INT_MIN<=val<=INT_MAX else (INT_MIN if val<0 else INT_MAX)
+        i = 0
+        if s[0] in '+-':
+            sign=-1 if s[0]=='-' else 1
+            i+=1
+        def recursion(i,current):
+            if i>=len(s) or not s[i].isdigit():
+                return sign*int(current)
+            current = current*10 + int(s[i])
+            if sign*current>INT_MAX: return INT_MAX
+            if sign*current<INT_MIN: return INT_MIN
+            return recursion(i+1,current)
+        current = 0
+        return recursion(i,current)
