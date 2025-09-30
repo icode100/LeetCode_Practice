ee3e19b694fec7140d160c7caa5c772965d46b93
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-09-30 11:29:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.70%) - LeetSync

--- Striver_SDE_SHEET/50-powx-n/powx-n.py
+++ Striver_SDE_SHEET/50-powx-n/powx-n.py
@@ -1,10 +1,9 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n<0:
-            x = 1/x
-            n = abs(n)
-        def recursion(n):
+        x,n = (x,n) if n>=0 else (1/x,-n)
+        if n==0: return 1
+        def recursion(x,n):
             if n==0: return 1
-            if n&1: return x*recursion(n-1)
-            else: return recursion(n>>1)**2
-        return recursion(n)
+            if n&1: return x*recursion(x,n-1)
+            else: return recursion(x,n>>1)**2
+        return recursion(x,n)
