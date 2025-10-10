bfb983b3629238863c2e91682b82acc311d6a0a0
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-10 18:28:45 +0530

    Time: 34 ms (38.53%) | Memory: 21.9 MB (10.16%) - LeetSync

--- Striver_SDE_SHEET/966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ Striver_SDE_SHEET/966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,9 +1,10 @@
 class Solution:
-    def numSubarraysWithSum(self, nums: List[int], k: int) -> int:
-        hashmap,ans,current = {},0,0
-        for n in nums:
-            current+=n
-            if current==k: ans+=1
-            ans+=hashmap.get(current-k,0)
-            hashmap[current] = hashmap.get(current,0)+1
-        return ans
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        hashmap = defaultdict(int)
+        sum,count = 0,0
+        for r,n in enumerate(nums):
+            sum+=n
+            if sum==goal:count+=1
+            count+=hashmap[sum-goal]
+            hashmap[sum]+=1
+        return count
