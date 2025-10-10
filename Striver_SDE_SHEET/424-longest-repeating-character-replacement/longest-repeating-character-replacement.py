0d9b551c46aec5ebde1122896c1bd4ed11d11831
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-10 18:12:11 +0530

    Time: 184 ms (22.16%) | Memory: 18 MB (49.53%) - LeetSync

--- Striver_SDE_SHEET/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ Striver_SDE_SHEET/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,14 +1,11 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        N = len(s)
-        counter = {}
-        l = 0
-        ans = 0
-        for r in range(N):
-            counter[s[r]] = counter.get(s[r], 0)+1
-            while l<r and counter and (r-l+1-max(counter.values()))>k:
-                counter[s[l]]-=1
+        hashset = defaultdict(int)
+        ans,l = 0,0
+        for r,c in enumerate(s):
+            hashset[c]+=1
+            while l<r and (r-l+1)-max(hashset.values())>k:
+                hashset[s[l]]-=1
                 l+=1
             ans = max(ans,r-l+1)
-        ans = max(ans,r-l+1)
         return ans
