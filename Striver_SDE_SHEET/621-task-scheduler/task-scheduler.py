fd03b4a30b91e1c88b70a339a201b41fb3d7e786
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-15 10:31:26 +0530

    Time: 11 ms (97.92%) | Memory: 19.2 MB (28.18%) - LeetSync

--- Striver_SDE_SHEET/621-task-scheduler/task-scheduler.py
+++ Striver_SDE_SHEET/621-task-scheduler/task-scheduler.py
@@ -1,14 +1,19 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        heap = list()
-        for key,val in Counter(tasks).items(): heappush(heap,-val)
-        q = deque()
-        time = 0
-        while heap or q:
-            time+=1
-            if heap:
-                freq = heappop(heap)
-                freq+=1
-                if freq!=0: q.append([freq,time+n])
-            if q and q[0][1]==time: heappush(heap,q.popleft()[0])
-        return time
+        if n==0:
+            return len(tasks)
+        c = Counter(tasks)
+        counter = sorted([[i,c[i]] for i in c],key=lambda x:-x[1])
+        mx = counter[0][1]   
+        ans = (n+1)*(mx-1)
+        for item in counter:
+            if item[1]==mx:
+                ans+=1
+        return max(ans,len(tasks))
+
+                
+
+
+
+
+
