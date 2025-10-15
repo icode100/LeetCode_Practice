1245a12c4dd217d5e25f07a1a5dd7c224219481a
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-15 15:45:23 +0530

    Time: 164 ms (75.44%) | Memory: 40.3 MB (59.37%) - LeetSync

--- Striver_SDE_SHEET/295-find-median-from-data-stream/find-median-from-data-stream.py
+++ Striver_SDE_SHEET/295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,18 +1,31 @@
 class MedianFinder:
 
     def __init__(self):
-        self.container = SortedList()
-
+        self.lheap = list()
+        self.rheap = list()
+        self.counter = 0
     def addNum(self, num: int) -> None:
-        self.container.add(num)
-        
+        if self.counter%2==0:
+            if self.rheap and num > -self.lheap[0] and num > self.rheap[0]:
+                heappush(self.rheap,num)
+                heappush(self.lheap,-heappop(self.rheap))
+            else:
+                heappush(self.lheap,-num)
+            self.counter+=1
+        else:
+            if num < -self.lheap[0]:
+                heappush(self.lheap,-num)
+                heappush(self.rheap,-heappop(self.lheap))
+            else:
+                heappush(self.rheap,num)
+            self.counter+=1
 
     def findMedian(self) -> float:
-        N = len(self.container)
-        if N&1:
-            return self.container[N//2]
+        if len(self.lheap)==len(self.rheap):
+            return (-self.lheap[0]+self.rheap[0])/2
         else:
-            return (self.container[(N//2)-1]+self.container[N//2])/2
+            return -self.lheap[0]
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:
