932bc55009ffebdb3bb8c65154b8d7863f52b065
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-22 15:35:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.51%) - LeetSync

---
+++ Striver_SDE_SHEET/678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        min,max = 0,0
+        for c in s:
+            if c=='(': 
+                min+=1
+                max+=1
+            elif c==')':
+                min-=1
+                max-=1
+            else:
+                min-=1
+                max+=1
+            if max<0: return False
+            if min<0: min=0
+        if 0 in range(min,max+1): return True
+        return False
