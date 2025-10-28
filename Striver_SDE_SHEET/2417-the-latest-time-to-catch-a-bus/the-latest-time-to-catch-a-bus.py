a45e6e79060c2445538118df02180a3b464b4439
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-28 20:22:22 +0530

    Time: 151 ms (16.02%) | Memory: 36.8 MB (75.24%) - LeetSync

--- Striver_SDE_SHEET/2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.py
+++ Striver_SDE_SHEET/2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.py
@@ -1,16 +1,17 @@
 class Solution:
     def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], capacity: int) -> int:
         buses.sort()
-        passengers = deque(sorted(passengers))
-        ans = -1
+        passengers.sort()
+        p = 0
+        ans = 0
         vis = set()
         for b in buses:
-            # if passengers[0]>b: continue
-            c = capacity
-            while passengers and c and passengers[0]<=b:
-                if passengers[0]-1 not in vis: ans = max(ans,passengers[0]-1)
-                vis.add(passengers.popleft())
-                c-=1
-            if c!=0:
-                if b not in vis: ans = max(ans,b)
+            cap = 0
+            while p<len(passengers) and passengers[p]<=b and cap<capacity:
+                if passengers[p]-1 not in vis: ans = max(passengers[p]-1,ans)
+                vis.add(passengers[p])
+                p+=1
+                cap+=1
+            if cap<capacity and b not in vis: ans = max(ans,b)
         return ans
+
