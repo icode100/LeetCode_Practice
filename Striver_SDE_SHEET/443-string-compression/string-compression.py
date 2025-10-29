ca297ab6db18f1a6d1aec0a1035c64c3dd030825
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-29 18:57:07 +0530

    Time: 2 ms (45.53%) | Memory: 17.8 MB (74.69%) - LeetSync

--- Striver_SDE_SHEET/443-string-compression/string-compression.py
+++ Striver_SDE_SHEET/443-string-compression/string-compression.py
@@ -1,21 +1,25 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        stack = list()
         prev = chars[0]
         count = 0
+        l = 0
         for i,c in enumerate(chars):
             if c==prev: count+=1
             else:
-                stack.append(prev)
+                chars[l] = prev
+                l+=1
                 if count>1: 
-                    for ch in str(count): stack.append(ch)
+                    for ch in str(count): 
+                        chars[l] = ch
+                        l+=1
                 prev = c
                 count = 1
 
-        stack.append(prev)
+        chars[l] = prev
+        l+=1        
         if count>1: 
-            for ch in str(count): stack.append(ch)
-        chars[::] = stack[::]
-        print(stack)
-        return len(stack)
+            for ch in str(count): 
+                chars[l] = ch
+                l+=1
+        return l
 
