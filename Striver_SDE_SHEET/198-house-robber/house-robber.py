9f14ee72a1ec1e6493604471f2f0937f025e323d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-30 13:18:36 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (13.11%) - LeetSync

--- Striver_SDE_SHEET/198-house-robber/house-robber.py
+++ Striver_SDE_SHEET/198-house-robber/house-robber.py
@@ -1,11 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp_i, dp_i_1, dp_i_2 = 0,0,0
-        for i in range(n-1,-1,-1):
-            dp_i = max(nums[i]+dp_i_2, dp_i_1)
-            dp_i_2 = dp_i_1
-            dp_i_1 = dp_i
-        return dp_i
-
-
+        @cache
+        def recursion(index):
+            if index>=len(nums): return 0
+            return max(recursion(index+1),nums[index]+recursion(index+2))
+        return recursion(0)
