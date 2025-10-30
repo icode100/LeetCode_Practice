3676ad8f1766f97852bdbc392425bab911fb499d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-30 12:21:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.31%) - LeetSync

--- Striver_SDE_SHEET/70-climbing-stairs/climbing-stairs.py
+++ Striver_SDE_SHEET/70-climbing-stairs/climbing-stairs.py
@@ -1,7 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0]*(n+2)
-        dp[n] = 1
+        # dp = [0]*(n+2)
+        dp_n = 1
+        dp_n_1 = 0
+        dp = 0
         for i in range(n-1,-1,-1):
-            dp[i] = dp[i+1]+dp[i+2]
-        return dp[0]
+            dp = dp_n+dp_n_1
+            dp_n_1 = dp_n
+            dp_n = dp
+        return dp
