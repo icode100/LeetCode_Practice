b2ef47def4c0c4f0f774c1c754cf378315642a94
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-10-30 11:24:07 +0530

    Time: 3 ms (19.80%) | Memory: 18.1 MB (60.65%) - LeetSync

--- Striver_SDE_SHEET/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ Striver_SDE_SHEET/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -4,28 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from heapq import *
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        hashmap = defaultdict(lambda: defaultdict(lambda:SortedList()))
-        self.minx,self.maxx = inf,-inf
-        def dfs(root,x,y):
-            if not root: return 
-            self.minx = min(self.minx,x)
-            self.maxx = max(self.maxx,x)
-            hashmap[x][y].add(root.val)
-            dfs(root.left,x-1,y+1)
-            dfs(root.right,x+1,y+1)
-        dfs(root,0,0)
-        ans = list()
-        for i in range(self.minx,self.maxx+1):
+        q = deque()
+        hashmap = defaultdict(lambda: defaultdict(list))
+        q.append((root,0,0))
+        while q:
+            n = len(q)
+            for _ in range(n):
+                node,col,row = q.popleft()
+                heappush(hashmap[col][row],node.val)
+                if node.left:q.append((node.left,col-1,row+1))
+                if node.right:q.append((node.right,col+1,row+1))
+        result = list()
+        for col in sorted(hashmap.keys()):
             temp = list()
-            for k in sorted(hashmap[i].keys()):
-                temp+=list(hashmap[i][k])
-            ans.append(temp.copy())
-        return ans
-                
-
-
-
-
-
+            for vals in hashmap[col].values():
+                while vals:
+                    temp.append(heappop(vals))
+            result.append(temp.copy())
+        return result
