b4c6dafff55b91848646e30974793c5f1d2998d1
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-01 17:47:10 +0530

    Time: 15 ms (7.01%) | Memory: 21.2 MB (6.66%) - LeetSync

--- Striver_SDE_SHEET/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ Striver_SDE_SHEET/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,9 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         N = len(prices)
-        @cache
-        def recursion(index,flag):
-            if index>=N: return 0
-            if flag: return max(prices[index]+recursion(index+1,not flag),recursion(index+1,flag))
-            else: return max(-prices[index]+recursion(index+1,not flag), recursion(index+1,flag))
-        return recursion(0,False)
+        dp = [[0,0] for _ in range(N+1)]
+        for index in range(N-1,-1,-1):
+            for flag in [True,False]:
+                if flag:
+                    dp[index][flag] = max(prices[index]+dp[index+1][not flag],dp[index+1][flag])
+                else:
+                    dp[index][flag] = max(-prices[index]+dp[index+1][not flag],dp[index+1][flag])
+        return dp[0][False]
+
