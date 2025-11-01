03560196499cb57eab6fb0701c06252bf2dfe702
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-01 18:16:10 +0530

    Time: 271 ms (33.07%) | Memory: 27.5 MB (34.04%) - LeetSync

---
+++ Striver_SDE_SHEET/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        N = len(prices)
+        dp = [[0,0] for _ in range(N+1)]
+        for index in range(N-1,-1,-1):
+            for flag in [True,False]:
+                if flag:
+                    dp[index][flag] = max(prices[index]+dp[index+1][not flag]-fee,dp[index+1][flag])
+                else:
+                    dp[index][flag] = max(-prices[index]+dp[index+1][not flag],dp[index+1][flag])
+        return dp[0][False]
