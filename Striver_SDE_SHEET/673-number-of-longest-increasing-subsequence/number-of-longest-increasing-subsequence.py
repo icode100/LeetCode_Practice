f70a82bb0eb254f41adffb27fc9f62e481f07cc8
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-01 20:27:10 +0530

    Time: 529 ms (42.07%) | Memory: 18.1 MB (46.25%) - LeetSync

--- Striver_SDE_SHEET/673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
+++ Striver_SDE_SHEET/673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -1,18 +1,14 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1]*n
-        count = [1]*n
-        lis,ans = 0,0
-        for i in range(n):
+        N = len(nums)
+        count = [1]*N
+        dp = [1]*N
+        for i in range(N):
             for j in range(i):
-                if nums[i]>nums[j]: 
-                    if 1+dp[j]>dp[i]:
-                        dp[i] = 1+dp[j]
+                if nums[i]>nums[j]:
+                    if dp[i]==dp[j]+1: count[i]+=count[j]
+                    elif dp[i]<dp[j]+1:
+                        dp[i] = dp[j]+1
                         count[i] = count[j]
-                    elif 1+dp[j]==dp[i]: count[i]+=count[j]                    
-            if dp[i]>lis:
-                lis = dp[i]
-                ans = count[i]
-            elif dp[i]==lis: ans+=count[i]
-        return ans
+        lis = max(dp)
+        return sum([count[i] if dp[i]==lis else 0 for i in range(N)])
