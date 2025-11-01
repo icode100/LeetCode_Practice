15c3da9b21c39d637dcb85ff292fd39bff6c4b19
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-01 18:15:07 +0530

    Time: 3 ms (73.76%) | Memory: 18.5 MB (57.36%) - LeetSync

---
+++ Striver_SDE_SHEET/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        N = len(prices)
+        dp = [[0,0] for _ in range(N+2)]
+        for index in range(N-1,-1,-1):
+            for flag in [True,False]:
+                if flag:
+                    dp[index][flag] = max(prices[index]+dp[index+2][not flag],dp[index+1][flag])
+                else:
+                    dp[index][flag] = max(-prices[index]+dp[index+1][not flag],dp[index+1][flag])
+        return dp[0][False]
