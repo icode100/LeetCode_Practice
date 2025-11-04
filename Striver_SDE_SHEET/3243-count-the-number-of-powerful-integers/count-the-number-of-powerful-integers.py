581dfbb787fd04627177698e1cfd6a1b7557d5ff
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-04 18:33:13 +0530

    Time: 27 ms (32.75%) | Memory: 18.4 MB (11.70%) - LeetSync

--- Striver_SDE_SHEET/3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.py
+++ Striver_SDE_SHEET/3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.py
@@ -1,23 +1,23 @@
 class Solution:
     def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
-        def leq(x):
-            if len(s)==len(x): return 1 if x>=s else 0
-            if len(s)>len(x): return 0
-
-            N = len(x)-len(s)
-            
-            ans = 0
-            for i in range(N):
-                if int(x[i])<=limit: ans += int(x[i]) * pow((limit+1),(N-i-1))
-                else: 
-                    ans += pow((limit+1),(N-i))
-                    return ans
-            if x[N:]>=s: ans+=1
-            return ans
+        N = len(str(finish))
+        prefix_length = N-len(s)
+        start = str(start).zfill(N)
+        finish = str(finish)
+        @cache
+        def recursion(index, ifstart, iffinish):
+            if index==N: return 1
+            low = int(start[index]) if ifstart else 0
+            high = int(finish[index]) if iffinish else 9
+            if index<prefix_length:
+                ans = 0
+                for num in range(low,min(high,limit)+1):
+                    ans+=recursion(index+1, ifstart and num==low, iffinish and num==high)
+                return ans
+            val = int(s[index-prefix_length])
+            if low<=val<=min(high,limit): 
+                return recursion(index+1,ifstart and val==low, iffinish and val==high)  
+            return 0
         
-        return leq(str(finish)) - leq(str(start-1))
-
-
-
-
-            
+        return recursion(0,True,True)
+                
