479b0ece76ef0eb145f41d4e134aa0645e768bc6
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-05 09:57:41 +0530

    Time: 415 ms (55.29%) | Memory: 42.5 MB (86.02%) - LeetSync

--- Striver_SDE_SHEET/1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ Striver_SDE_SHEET/1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -7,24 +7,19 @@ class Solution:
                 if str1[i-1]==str2[j-1]: dp[i][j] = dp[i-1][j-1]+1
                 else: dp[i][j] = max(dp[i-1][j],dp[i][j-1])
         ans = ""
-        i=N
-        j=M
+        i,j = N,M
         while i>0 and j>0:
-            if str1[i-1]==str2[j-1]:
+            if str1[i-1]==str2[j-1]: 
+                ans+=str1[i-1]
+                i,j = i-1,j-1
+            elif dp[i-1][j]>dp[i][j-1]:
                 ans+=str1[i-1]
                 i-=1
-                j-=1
             else:
-                if dp[i-1][j]>dp[i][j-1]:
-                    ans+=str1[i-1]
-                    i-=1
-                else:
-                    ans+=str2[j-1]
-                    j-=1
-        while i>0:
-            ans+=str1[i-1]
-            i-=1
-        while j>0:
-            ans+=str2[j-1]
-            j-=1
+                ans+=str2[j-1]
+                j-=1
+        for k in range(j,0,-1):ans+=str2[k-1]
+        for k in range(i,0,-1):ans+=str1[k-1]
         return ans[::-1]
+
+                
