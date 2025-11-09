3ee14f2db20c8695b25c7eee3419396ddb07d4ec
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-09 18:22:28 +0530

    Time: 5 ms (98.69%) | Memory: 17.7 MB (80.39%) - LeetSync

--- Striver_SDE_SHEET/3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.py
+++ Striver_SDE_SHEET/3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.py
@@ -1,19 +1,17 @@
 class Solution:
     def minimumMoves(self, grid: List[List[int]]) -> int:
-        m,n = len(grid), len(grid[0])
-        zero = [[i,j] for i in range(m) for j in range(n) if grid[i][j]==0]
-        extra = [[i,j,grid[i][j]-1] for i in range(m) for j in range(n) if grid[i][j]>1]
-        compute_manhattan = lambda x,y: abs(x[0]-y[0])+abs(x[1]-y[1])
-        ans = sys.maxsize
-        def dfs(i,count):
-            nonlocal ans
-            if i==len(zero): 
-                ans = min(ans,count)
-                return
-            for j in range(len(extra)):
-                if extra[j][2]>0:
-                    extra[j][2]-=1
-                    dfs(i+1,count+compute_manhattan(extra[j],zero[i]))
-                    extra[j][2]+=1
-        dfs(0,0)
-        return ans
+        zeros = [(i,j) for i in range(3) for j in range(3) if grid[i][j]==0]
+        extras = [(i,j) for i in range(3) for j in range(3) if grid[i][j]>1]
+        self.ans = int(1e9)
+        def rec(i,dis):
+            if i==len(zeros):
+                self.ans = min(self.ans,dis)
+                return 
+            r,c = zeros[i]
+            for nr,nc in extras:
+                if grid[nr][nc]>1:
+                    grid[nr][nc]-=1
+                    rec(i+1,dis+abs(r-nr)+abs(c-nc))
+                    grid[nr][nc]+=1
+        rec(0,0)
+        return self.ans
