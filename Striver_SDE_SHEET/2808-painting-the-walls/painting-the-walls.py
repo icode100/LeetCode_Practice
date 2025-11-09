7e8654c953e6c1c5b7c6921c471f6479806d0455
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-09 13:39:40 +0530

    Time: 1857 ms (44.99%) | Memory: 433.8 MB (44.98%) - LeetSync

--- Striver_SDE_SHEET/2808-painting-the-walls/painting-the-walls.py
+++ Striver_SDE_SHEET/2808-painting-the-walls/painting-the-walls.py
@@ -7,5 +7,5 @@ class Solution:
         def rec(i,C):
             if C<=0: return 0
             if i>=N: return int(1e9)
-            return min(value[i]+rec(i+1,C-weight[i]),rec(i+1,C))
+            return min(value[i]+rec(i+1,C-weight[i]) if weight[i]<=C else value[i],rec(i+1,C))
         return rec(0,C)
