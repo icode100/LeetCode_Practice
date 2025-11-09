2f444cf90db976525f7cf1576f0f01f91b5e894a
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-09 13:38:26 +0530

    Time: 1885 ms (44.34%) | Memory: 435.8 MB (43.69%) - LeetSync

---
+++ Striver_SDE_SHEET/2808-painting-the-walls/painting-the-walls.py
@@ -0,0 +1,11 @@
+class Solution:
+    def paintWalls(self, cost: List[int], time: List[int]) -> int:
+        weight = [t+1 for t in time]
+        value = cost
+        N = C = len(time)  
+        @cache 
+        def rec(i,C):
+            if C<=0: return 0
+            if i>=N: return int(1e9)
+            return min(value[i]+rec(i+1,C-weight[i]),rec(i+1,C))
+        return rec(0,C)
