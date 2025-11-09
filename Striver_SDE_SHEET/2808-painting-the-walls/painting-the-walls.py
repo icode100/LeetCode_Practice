7c0b039deecc5b616614934289deabf4998778f8
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-09 13:53:50 +0530

    Time: 823 ms (90.62%) | Memory: 25.4 MB (57.28%) - LeetSync

--- Striver_SDE_SHEET/2808-painting-the-walls/painting-the-walls.py
+++ Striver_SDE_SHEET/2808-painting-the-walls/painting-the-walls.py
@@ -3,9 +3,10 @@ class Solution:
         weight = [t+1 for t in time]
         value = cost
         N = C = len(time)  
-        @cache 
-        def rec(i,C):
-            if C==0: return 0
-            if i>=N: return int(1e9)
-            return min(value[i]+rec(i+1,C-weight[i]) if weight[i]<=C else value[i],rec(i+1,C))
-        return rec(0,C)
+        dp = [[0 for _ in range(C+1)] for _ in range(N+1)]
+        for i in range(C+1): dp[N][i] = int(1e9)
+        for i in range(N+1): dp[i][0] = 0
+        for i in range(N-1,-1,-1):
+            for c in range(1,C+1):
+                dp[i][c] = min(value[i]+dp[i+1][c-weight[i]] if weight[i]<=c else value[i], dp[i+1][c])
+        return dp[0][C]
