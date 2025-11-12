3eb1c04387217f4498bb56ed61a8cadf6ecc1f4a
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-12 15:42:25 +0530

    Time: 33 ms (45.92%) | Memory: 19.2 MB (75.42%) - LeetSync

--- Striver_SDE_SHEET/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ Striver_SDE_SHEET/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,29 +1,15 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        m, n = len(heights), len(heights[0])
-        pacific = [[False] * n for _ in range(m)]
-        altantic = [[False] * n for _ in range(m)]
-        ans = list()
-        for i in range(m):
-            self.dfs(heights, pacific, i, 0, -sys.maxsize-1)
-            self.dfs(heights, altantic, i, n-1, -sys.maxsize-1)
-        for j in range(n):
-            self.dfs(heights, pacific, 0, j, -sys.maxsize-1)
-            self.dfs(heights, altantic, m-1, j, -sys.maxsize-1)
-        for i in range(m):
-            for j in range(n):
-                if pacific[i][j] and altantic[i][j]:
-                    ans.append([i, j])
-        return ans
-        
-    def dfs(self, heights: List[List[int]], dp: List[List[bool]], i: int, j: int, pre: int) -> None:
-        m, n = len(heights), len(heights[0])
-        if (i < 0 or j < 0 or i >= m or j >= n or dp[i][j] or heights[i][j] < pre):
-            return
-        dp[i][j] = True
-        self.dfs(heights, dp, i-1, j, heights[i][j])
-        self.dfs(heights, dp, i+1, j, heights[i][j])
-        self.dfs(heights, dp, i, j-1, heights[i][j])
-        self.dfs(heights, dp, i, j+1, heights[i][j])
-
-    
+        N,M = len(heights),len(heights[0])
+        def recursion(r,c,vis):
+            vis.add((r,c))
+            for nr,nc in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
+                if 0<=nr<N and 0<=nc<M and (nr,nc) not in vis and heights[nr][nc]>=heights[r][c]:
+                    recursion(nr,nc,vis)
+        p = set()
+        a = set()
+        for i in range(N):
+            for j in range(M):
+                if i==0 or j==0: recursion(i,j,p)
+                if i==N-1 or j==M-1: recursion(i,j,a)
+        return list(a&p)
