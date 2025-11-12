3ab66c05a84c3f8332eb1eecc9c95ebdb1409d4f
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-12 10:05:15 +0530

    Time: 15 ms (26.00%) | Memory: 24.3 MB (9.03%) - LeetSync

--- Striver_SDE_SHEET/740-delete-and-earn/delete-and-earn.py
+++ Striver_SDE_SHEET/740-delete-and-earn/delete-and-earn.py
@@ -1,13 +1,8 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         counter = Counter(nums)
-        n = max(counter.keys())
-        dp = [0] * (n + 1)
-        for i in range(1, n + 1):
-            pick = counter[i] * i + (dp[i - 2] if i - 2 >= 0 else 0)
-            notpick = dp[i - 1] if i - 1 >= 0 else 0
-            dp[i] = max(pick, notpick)
-        return dp[n]
-
-
-        
+        @cache
+        def recursion(num):
+            if num<=0: return 0
+            return max(counter[num]*num+recursion(num-2), recursion(num-1))
+        return recursion(max(nums))
