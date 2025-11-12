2f1cdcc7868d7b62809dd107fea11227715d5162
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-12 16:45:20 +0530

    Time: 53 ms (44.80%) | Memory: 23.7 MB (48.34%) - LeetSync

--- Striver_SDE_SHEET/820-find-eventual-safe-states/find-eventual-safe-states.py
+++ Striver_SDE_SHEET/820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,23 +1,20 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        vis,pathvis = set(),set()
-        check = defaultdict(lambda: True)
-        def dfs(node):
-            vis.add(node)
-            pathvis.add(node)
-            for ngh in graph[node]:
-                if ngh not in vis:
-                    if not dfs(ngh):
-                        print(node)
-                        check[node] = False
-                        return False
-                elif ngh in pathvis:
-                    check[node] = False
-                    return False
-            pathvis.remove(node)
-            return True
-        for u in range(len(graph)):
-            if u not in vis: dfs(u)
-            else: vis.add(u)
-        # print(dict(check))
-        return [u for u in range(len(graph)) if check[u]==True]
+        V = len(graph)
+        rev = defaultdict(list)
+        indegree = [0]*V
+        ans = list()
+        for u in range(V):
+            for v in graph[u]:
+                rev[v].append(u)
+                indegree[u]+=1
+        q = deque()
+        for v in range(V):
+            if indegree[v]==0: q.append(v)
+        while q:
+            node = q.popleft()
+            ans.append(node)
+            for ngh in rev[node]:
+                indegree[ngh]-=1
+                if indegree[ngh]==0: q.append(ngh)
+        return sorted(ans)
