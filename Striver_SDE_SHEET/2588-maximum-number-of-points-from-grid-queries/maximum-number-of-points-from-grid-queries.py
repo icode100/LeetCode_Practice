f1038269082ad29009f2176ecab7b061b724bf3d
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-12 13:23:14 +0530

    Time: 607 ms (59.11%) | Memory: 43 MB (20.20%) - LeetSync

--- Striver_SDE_SHEET/2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.py
+++ Striver_SDE_SHEET/2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.py
@@ -1,29 +1,18 @@
-from heapq import heappop, heappush
-from typing import List
-
 class Solution:
     def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
-        m, n = len(grid), len(grid[0])
-        k = len(queries)
-        
-        heap = [(grid[0][0], 0, 0)]  
-        qr = sorted((num, i) for i, num in enumerate(queries))  
-        ans = [0] * k
-        vis = {(0, 0)}
-        
-        current = 0  
-        
-        for q, i in qr:
-            while heap and heap[0][0] < q:
-                node, r, c = heappop(heap)
-                current += 1
-                for nr, nc in [(r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1)]:
-                    if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in vis:
-                        heappush(heap, (grid[nr][nc], nr, nc))  
-                        vis.add((nr, nc))
-            ans[i] = current
-        
-        return ans
-
-                
-
+        N,M = len(grid),len(grid[0])
+        heap = [(grid[0][0],0,0)]
+        vis = {(0,0)}
+        count = 0
+        ans = defaultdict(int)
+        for q in sorted(queries):
+            while heap and heap[0][0]<q:
+                val,r,c = heappop(heap)
+                count+=1
+                for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
+                    nr,nc = r+dr,c+dc
+                    if 0<=nr<N and 0<=nc<M and (nr,nc) not in vis:
+                        heappush(heap,(grid[nr][nc],nr,nc))
+                        vis.add((nr,nc))
+            ans[q] = count
+        return [ans[q] for q in queries]
