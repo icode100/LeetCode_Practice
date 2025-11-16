ed220c53cfe62b6700acd6f8332fb78cd1d95efd
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-16 22:05:40 +0530

    Time: 1633 ms (26.34%) | Memory: 34 MB (32.47%) - LeetSync

--- Striver_SDE_SHEET/416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ Striver_SDE_SHEET/416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,14 +1,20 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums)&1: return False
+        target = sum(nums)//2
         N = len(nums)
-        total = sum(nums)
-        if total&1: return False
-        target = total//2
-        mask = 1<<nums[0]
-        for i in range(1,N):
-            mask = (mask<<nums[i])|mask
-            mask |= 1<<nums[i]
-            if mask&(1<<target): return True
-        return False
-            
-                
+        dp = [[False for _ in range(target+1)] for _ in range(N+1)]
+        for i in range(N+1):dp[i][0] = True
+        for i in range(1,N+1):
+            for tar in range(1,target+1):
+                dp[i][tar] = dp[i-1][tar] or (dp[i-1][tar-nums[i-1]] if nums[i-1]<=tar else False)
+        return dp[N][target]
+
+        # @cache
+        # def recursion(index,target):
+        #     if target==0: return True
+        #     if index<0: return False
+        #     return recursion(index-1,target) or (recursion(index-1,target-nums[index]) if nums[index]<=target else False)
+        # return recursion(N-1,target)
+
+
