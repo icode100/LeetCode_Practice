a05e11ad3d91eebf671c570c011f0c5452aaa530
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-16 20:06:39 +0530

    Time: 7 ms (14.00%) | Memory: 18.2 MB (97.75%) - LeetSync

--- Striver_SDE_SHEET/801-is-graph-bipartite/is-graph-bipartite.py
+++ Striver_SDE_SHEET/801-is-graph-bipartite/is-graph-bipartite.py
@@ -3,15 +3,21 @@ class Solution:
         vis = defaultdict(int)
         N = len(graph)
         
-        def dfs(node,col):
-            vis[node] = col
-            for ngh in graph[node]:
-                if ngh not in vis: 
-                    if dfs(ngh,1-col)==False: return False
-                elif vis[ngh] == col: return False
+        def check(node):
+            q = deque([node])
+            while q:
+                k = len(q)
+                for _ in range(k):
+                    node = q.popleft()
+                    for ngh in graph[node]:
+                        if ngh not in vis:
+                            vis[ngh]=1-vis[node]
+                            q.append(ngh)
+                        elif vis[ngh]==vis[node]: return False
             return True
             
         for u in range(N):
             if u not in vis:
-                if dfs(u,0)==False: return False
+                vis[u] = 0
+                if check(u)==False: return False
         return True
