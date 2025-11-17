b6965b4199aa39a7964661db9bd77a6a62cb0af8
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-17 13:20:06 +0530

    Time: 13 ms (36.91%) | Memory: 18.2 MB (79.89%) - LeetSync

--- Striver_SDE_SHEET/32-longest-valid-parentheses/longest-valid-parentheses.py
+++ Striver_SDE_SHEET/32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -1,13 +1,15 @@
 class Solution:
-    def longestValidParentheses(self, s):
-        # dp[i] = Longest valid parantheses till index i
-        if s=='': return 0
-        dp = [0]*len(s)
-        for i,c in enumerate(s):
-            if c==')':
-                if i>0 and s[i-1]=='(': 
-                    dp[i] =(dp[i-2]+2) if i-2>0 else 2
-                elif i-dp[i-1]>0 and s[i-dp[i-1]-1]=='(': 
-                    dp[i] =(dp[i-1]+dp[i-dp[i-1]-2]+2) if i-dp[i-1]-2>=0 else (dp[i-1]+2)
-        print(dp)
-        return max(dp)
+    def longestValidParentheses(self, s: str) -> int:
+        l,r,ans = 0,0,0
+        for c in s:
+            if c=='(': l+=1
+            else: r+=1
+            if l==r: ans = max(ans,r*2)
+            if r>l: r=l=0
+        l=r=0
+        for c in s[::-1]:
+            if c=='(': l+=1
+            else: r+=1
+            if l==r: ans = max(ans,r*2)
+            if l>r: l=r=0
+        return ans
