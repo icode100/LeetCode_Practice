a70e6e1432657d7ff483047a64a0a0180ca313f0
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-24 13:59:11 +0530

    Time: 161 ms (18.18%) | Memory: 28.3 MB (15.25%) - LeetSync

--- Striver_SDE_SHEET/1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
+++ Striver_SDE_SHEET/1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
@@ -2,19 +2,31 @@ class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
         N = len(nums)
 
-        @cache
-        def recursion(index,mod):
+        dp = [[0]*3 for _ in range(N+1)]
+        dp[0][1] = dp[0][2] = -inf
 
-            if index<0: 
-                return 0 if mod==0 else -inf
+        for i in range(1,N+1):
+            for mod in range(3):
+                notpick = dp[i-1][mod]
+                pick = nums[i-1]+dp[i-1][(mod+(nums[i-1]%3))%3]
+                dp[i][mod] = max(notpick,pick)
+        
+        return dp[N][0] if dp[N][0]!=-inf else 0
 
-            notpick = recursion(index-1,mod)
-            pick = nums[index]+recursion(index-1,(mod+(nums[index]%3))%3)
 
-            return max(pick,notpick)
+        # @cache
+        # def recursion(index,mod):
 
-        ans = recursion(N-1,0)
-        return ans if ans != -inf else 0
+        #     if index<0: 
+        #         return 0 if mod==0 else -inf
+
+        #     notpick = recursion(index-1,mod)
+        #     pick = nums[index]+recursion(index-1,(mod+(nums[index]%3))%3)
+
+        #     return max(pick,notpick)
+
+        # ans = recursion(N-1,0)
+        # return ans if ans != -inf else 0
 
     
             
