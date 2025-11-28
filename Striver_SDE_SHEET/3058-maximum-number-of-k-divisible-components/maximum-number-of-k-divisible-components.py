dce708b10ca8b2e0f39c32bfe9ca6b14c7b41b23
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-11-28 09:33:54 +0530

    Time: 105 ms (87.68%) | Memory: 39.6 MB (55.80%) - LeetSync

--- Striver_SDE_SHEET/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
+++ Striver_SDE_SHEET/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -1,30 +1,20 @@
 class Solution:
-    def maxKDivisibleComponents(self, N: int, edges: List[List[int]], values: List[int], k: int) -> int:
-
-        graph = [list() for _ in range(N)]
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        graph = defaultdict(list)
         for u,v in edges:
             graph[u].append(v)
             graph[v].append(u)
-
-        
-        def dfs(node,parent):
-            
-            current = 0
+        ans = 0
+        def recursion(node,parent):
+            nonlocal ans
+            ret = values[node]
             for ngh in graph[node]:
-                if ngh != parent:
-
-                    k_comp,val = dfs(ngh,node)
-                    values[node]+=val
-                    current+=k_comp
-            
-            if values[node]%k == 0:
-                current+=1
-                values[node] = 0
-            
-            return current,values[node]
-        
-        return dfs(0,-1)[0]
-
-            
+                if ngh != parent: ret+=recursion(ngh,node)
+            if ret%k==0:
+                ans+=1
+                ret = 0
+            return ret
+        recursion(0,-1)
+        return ans
 
             
