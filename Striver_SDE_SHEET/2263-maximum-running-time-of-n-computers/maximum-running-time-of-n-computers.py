c4bf9c5683bca153af6f75d1ebfd6473eb619172
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-12-01 20:15:35 +0530

    Time: 1421 ms (36.67%) | Memory: 31.1 MB (53.81%) - LeetSync

--- Striver_SDE_SHEET/2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
+++ Striver_SDE_SHEET/2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
-        batteries.sort()
-        extra = sum(batteries[:-n])
-        live = batteries[-n:]
 
-        for i in range(n-1):
-            if (live[i+1]-live[i])*(i+1) > extra:
-                return live[i]+extra//(i+1)
+        def check(mid):
+            current = sum(min(p,mid) for p in batteries)
+            return current//n >= mid
 
-            extra-=(i+1)*(live[i+1]-live[i])
+        l,r = 1,sum(batteries)//n
+
+        while l<r:
+            mid = r - (r-l)//2
+
+            if check(mid): l = mid
+            else: r = mid-1
         
-        return live[-1] + extra//n
+        return l
