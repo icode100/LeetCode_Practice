149b9691d7b86c67f984a9d54d4af4b72d27e7fc
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-12-10 11:46:57 +0530

    Time: 207 ms (15.00%) | Memory: 32.2 MB (17.22%) - LeetSync

--- Striver_SDE_SHEET/3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.py
+++ Striver_SDE_SHEET/3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.py
@@ -1,10 +1,3 @@
 class Solution:
-    def __init__(self):
-        self.mod = int(1e9+7)
     def countPermutations(self, complexity: List[int]) -> int:
-        if min(complexity)!=complexity[0]: return 0
-        if complexity.count(complexity[0])>1: return 0
-
-        N = len(complexity)
-        
-        return factorial(N-1)%self.mod
+        return 0 if (min(complexity)!=complexity[0]) or (complexity.count(complexity[0])>1) else factorial(len(complexity)-1)%int(1e9+7)
