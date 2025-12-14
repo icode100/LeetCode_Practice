2cc296bc35be9ab895069b8fca7c1639af0b3aca
Ipsit Das <121373392+icode100@users.noreply.github.com>
2025-12-14 22:57:04 +0530

    Time: 333 ms (42.86%) | Memory: 18.1 MB (84.76%) - LeetSync

--- Striver_SDE_SHEET/2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
+++ Striver_SDE_SHEET/2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
@@ -4,35 +4,26 @@ class Solution:
     def numberOfWays(self, corridor: str) -> int:
         N:int = len(corridor)
 
-        if corridor.count('S') % 2 != 0 or corridor.count('S') == 0:
+        count:int = 0
+        ans:int = 1
+        i:int = 0
+        if corridor.count('S') == 0 or corridor.count('S') % 2 != 0:
             return 0
-        dp = [[0,0,1] for _ in range(N+1)]
-        for index in range(N-1,-1,-1):
-            for seats in range(3):
-                if seats==2:
-                    if corridor[index]=='S': dp[index][seats] = dp[index+1][1] % self.mod
-                    else: dp[index][seats] = (dp[index+1][0]+dp[index+1][2])%self.mod
-
-                else:
-                    if corridor[index]=='S': dp[index][seats] = dp[index+1][seats+1] % self.mod
-                    else: dp[index][seats] = dp[index+1][seats] % self.mod
+        target = corridor.count('S')//2
         
-        return dp[0][0]
-        # @cache
-        # def recursion(index:int, seats:int)->int:
-        #     if index==N: return 1 if seats==2 else 0
+        while i<N:
+            if corridor[i]=='S':
+                count+=1
+            if count==2:
+                target-=1
+                i+=1
+                temp = 0
+                while i<N and corridor[i]=='P':
+                    temp+=1
+                    i+=1
+                if target>0: ans = (ans*(temp+1))%self.mod
+                count = 0
+                continue
+            i+=1
 
-        #     if seats==2:
-        #         if corridor[index]=='S': return recursion(index+1,1) % self.mod
-        #         else: return (recursion(index+1,0)+recursion(index+1,2))%self.mod
-            
-        #     else:
-        #         if corridor[index]=='S': return recursion(index+1,seats+1) % self.mod
-        #         else: return recursion(index+1,seats) % self.mod
-
-        
-        # return recursion(0,0) % self.mod
-
-
-
-
+        return ans
