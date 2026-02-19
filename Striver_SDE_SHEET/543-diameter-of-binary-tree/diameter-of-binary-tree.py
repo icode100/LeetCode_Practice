beb40a42f545a06d72fa969ece92a6e1b12376ba
Ipsit Das <121373392+icode100@users.noreply.github.com>
2026-02-19 17:26:52 +0530

    Time: 3 ms (80.04%) | Memory: 22.5 MB (14.55%) - LeetSync

---
+++ Striver_SDE_SHEET/543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.ans = 0
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root: return 0
+            lefth,righth = dfs(root.left),dfs(root.right)
+            self.ans = max(self.ans,lefth+righth)
+            return max(lefth,righth)+1
+        dfs(root)
+        return self.ans
