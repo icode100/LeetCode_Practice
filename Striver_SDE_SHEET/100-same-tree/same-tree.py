898caf6cdca750832aa17856e8ce1b32e5818211
Ipsit Das <121373392+icode100@users.noreply.github.com>
2026-02-19 17:34:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (71.23%) - LeetSync

--- Striver_SDE_SHEET/100-same-tree/same-tree.py
+++ Striver_SDE_SHEET/100-same-tree/same-tree.py
@@ -6,10 +6,9 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def dfs(r1,r2):
-            if not r1 and not r2: return True
-            if not r1: return False
-            if not r2: return False
-            if r1.val==r2.val and dfs(r1.left,r2.left) and dfs(r1.right,r2.right): return True
-            return False
-        return dfs(p,q) 
+        def dfs(p,q):
+            if p==None and q==None: return True
+            if p==None or q==None: return False
+            if p.val!=q.val: return False
+            return dfs(p.left,q.left) and dfs(p.right,q.right)
+        return dfs(p,q)
