128cadbe162de158e7e532d844e63437b4576d50
Ipsit Das <121373392+icode100@users.noreply.github.com>
2026-02-20 10:28:05 +0530

    Time: 1794 ms (55.94%) | Memory: 464.5 MB (33.48%) - LeetSync

--- Striver_SDE_SHEET/2808-painting-the-walls/painting-the-walls.py
+++ Striver_SDE_SHEET/2808-painting-the-walls/painting-the-walls.py
@@ -1,12 +1,17 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        weight = [t+1 for t in time]
-        value = cost
-        N = C = len(time)  
-        dp = [[0 for _ in range(C+1)] for _ in range(N+1)]
-        for i in range(C+1): dp[N][i] = int(1e9)
-        for i in range(N+1): dp[i][0] = 0
-        for i in range(N-1,-1,-1):
-            for c in range(1,C+1):
-                dp[i][c] = min(value[i]+dp[i+1][c-weight[i]] if weight[i]<=c else value[i], dp[i+1][c])
-        return dp[0][C]
+        C = len(cost)
+        N = len(cost)
+        value = [c for c in cost]
+        weight = list(map(lambda x:x+1,time))
+        
+        @cache
+        def dp(i,C):
+            if C<=0: return 0
+            if i<0: return inf
+
+            return min(
+                value[i]+dp(i-1,C-weight[i]) if C-weight[i]>=0 else value[i], #pick 
+                dp(i-1,C) # not-pick
+            )
+        return dp(N-1,C)
