0b9cc9edfaee07cc4e04f4d8ee4be1e5947fa5f3
Ipsit Das <121373392+icode100@users.noreply.github.com>
2026-02-23 00:18:00 +0530

    Time: 203 ms (10.71%) | Memory: 62.1 MB (7.14%) - LeetSync

--- Striver_SDE_SHEET/3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
+++ Striver_SDE_SHEET/3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -1,33 +1,12 @@
-class Solution(object):
-    def maximumValueSum(self, nums, k, edges):
-        """
-        :type nums: List[int]
-        :type k: int
-        :type edges: List[List[int]]
-        :rtype: int
-        """
-        n = len(nums)
-        after = []
-        for i in range(n):
-            t = nums[i] ^ k
-            after.append([t - nums[i], nums[i], t])
-        after.sort(key = lambda x:x[0])
-        def summ(array, ind):
-            tot = 0
-            for lists in array:
-                tot += lists[ind]
-            return tot
-        if after[-1][0] < 0:
-            return summ(after, 1)
-        if after[0][0] >= 0:
-            if n % 2 == 0:
-                return summ(after, 2)
-            return summ(after[1:], 2) + summ(after[:1], 1)
-        l = bisect.bisect_left(after, [0, -1, -1])
-        if after[l][0] < 0:
-            l += 1
-        if (n - l) % 2 == 0:
-            return summ(after[l:], 2) + summ(after[:l], 1)
-        if after[l - 1][0] + after[l][0] > 0:
-            return summ(after[l - 1:], 2) + summ(after[:l - 1], 1)
-        return summ(after[l + 1:], 2) + summ(after[:l + 1], 1)
+class Solution:
+    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+
+        @cache
+        def recursion(i,p):
+            if i==len(nums):
+                return 0 if p==0 else -int(1e9)
+            return max(
+                (nums[i]^k)+recursion(i+1,p^1),
+                nums[i]+recursion(i+1,p)
+            )
+        return recursion(0,0)
