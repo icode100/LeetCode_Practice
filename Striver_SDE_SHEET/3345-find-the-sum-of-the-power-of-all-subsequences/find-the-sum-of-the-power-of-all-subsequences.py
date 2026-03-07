f429c0850c4cdd70c7656aa809ce51e3b1c4f089
Ipsit Das <121373392+icode100@users.noreply.github.com>
2026-03-07 18:10:06 +0530

    Time: 471 ms (30.00%) | Memory: 138.5 MB (13.33%) - LeetSync

--- Striver_SDE_SHEET/3345-find-the-sum-of-the-power-of-all-subsequences/find-the-sum-of-the-power-of-all-subsequences.py
+++ Striver_SDE_SHEET/3345-find-the-sum-of-the-power-of-all-subsequences/find-the-sum-of-the-power-of-all-subsequences.py
@@ -2,7 +2,7 @@ class Solution:
     def sumOfPower(self, nums: List[int], k: int) -> int:
         mod = int(1e9+7)
         N = len(nums)
-        
+
         @cache
         def recursion(i,current,M):
             if current==0:
@@ -16,5 +16,5 @@ class Solution:
                     recursion(i+1,current-nums[i],M+1) if nums[i]<=current else 0
                 ]
             )%mod
-        
+        recursion.cache_clear()
         return recursion(0, k, 0)
